08f4508a5199440d7c21dbe8521e596610a23c10
Liubov Korshunova <lkorshunova@at-consulting.ru>
2015-11-23 14:37:18 +0300

    exc

--- src/Matrix.java
+++ src/Matrix.java
@@ -32,40 +32,40 @@ public class Matrix {
     public static Matrix read(BufferedReader br) throws IllegalArgumentException, IOException {
         String input;
 
-        if ((input = br.readLine()) != null) {
-            String[] dimensions = input.split(" ");
-            if (dimensions.length != 2) {
-                throw new IllegalArgumentException("Wrong number of dimensions.");
-            }
-            int x = Integer.parseInt(dimensions[0]);
-            int y = Integer.parseInt(dimensions[1]);
-            List<Token>[][] matrix = new ArrayList[x][y];
-            for (int i = 0; i < x; i++) {
-                for (int j = 0; j < y; j++) {
-                    if ((input = br.readLine()) != null) {
-                        String[] values = input.split(" ");
-                        List<Token> tokens = matrix[i][j];
-                        for (String value : values) {
-                            char c = value.charAt(0);
-                            if (c >= '0' && c <= '9') {
-                                Number num = new Number(Integer.parseInt(value));
-                                tokens.add(num);
-                            } else if (c >= 'A' && c <= 'Z') {
-                                Reference ref = new Reference();
-                                ref.setX(c - 'A');
-                                ref.setY(Integer.parseInt(value.substring(1)) - 1);
-                                tokens.add(ref);
-                            } else if (c == '+' || c == '-' || c == '*' || c == '/') {
-                                Operator op = new Operator(Operator.OperatorTypeEnum.lookupSymbol(value));
-                                tokens.add(op);
-                            }
+        if ((input = br.readLine()) == null) {
+            throw new IllegalArgumentException("Empty input.");
+        }
+        String[] dimensions = input.split(" ");
+        if (dimensions.length != 2) {
+            throw new IllegalArgumentException("Wrong number of dimensions.");
+        }
+        int x = Integer.parseInt(dimensions[0]);
+        int y = Integer.parseInt(dimensions[1]);
+        List<Token>[][] matrix = new ArrayList[x][y];
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                if ((input = br.readLine()) != null) {
+                    String[] values = input.split(" ");
+                    List<Token> tokens = matrix[i][j];
+                    for (String value : values) {
+                        char c = value.charAt(0);
+                        if (c >= '0' && c <= '9') {
+                            Number num = new Number(Integer.parseInt(value));
+                            tokens.add(num);
+                        } else if (c >= 'A' && c <= 'Z') {
+                            Reference ref = new Reference();
+                            ref.setX(c - 'A');
+                            ref.setY(Integer.parseInt(value.substring(1)) - 1);
+                            tokens.add(ref);
+                        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+                            Operator op = new Operator(Operator.OperatorTypeEnum.lookupSymbol(value));
+                            tokens.add(op);
                         }
                     }
                 }
             }
-            return new Matrix(matrix);
         }
-        return new Matrix(new ArrayList[0][0]);
+        return new Matrix(matrix);
     }
     public void print() {
         for (int i = 0; i < content.length; i++) {
