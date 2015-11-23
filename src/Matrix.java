b475440be96264d3be789148ca83fd46d3d0b5b1
Liubov Korshunova <lkorshunova@at-consulting.ru>
2015-11-23 14:46:58 +0300

    defineToken

--- src/Matrix.java
+++ src/Matrix.java
@@ -51,23 +51,27 @@ public class Matrix {
                 String[] values = input.split(" ");
                 List<Token> tokens = matrix[i][j];
                 for (String value : values) {
-                    char c = value.charAt(0);
-                    if (c >= '0' && c <= '9') {
-                        Number num = new Number(Integer.parseInt(value));
-                        tokens.add(num);
-                    } else if (c >= 'A' && c <= 'Z') {
-                        Reference ref = new Reference();
-                        ref.setX(c - 'A');
-                        ref.setY(Integer.parseInt(value.substring(1)) - 1);
-                        tokens.add(ref);
-                    } else if (c == '+' || c == '-' || c == '*' || c == '/') {
-                        Operator op = new Operator(Operator.OperatorTypeEnum.lookupSymbol(value));
-                        tokens.add(op);
-                    }
+                    tokens.add(defineToken(value));
                 }
             }
         }
         return new Matrix(matrix);
+    }
+
+    private static Token defineToken(String value) throws IllegalArgumentException {
+        char c = value.charAt(0);
+        if (c >= '0' && c <= '9') {
+            return new Number(Integer.parseInt(value));
+        } else if (c >= 'A' && c <= 'Z') {
+            Reference ref = new Reference();
+            ref.setX(c - 'A');
+            ref.setY(Integer.parseInt(value.substring(1)) - 1);
+            return ref;
+        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+            return new Operator(Operator.OperatorTypeEnum.lookupSymbol(value));
+        } else {
+            throw new IllegalArgumentException("Wrong token type.");
+        }
     }
     public void print() {
         for (int i = 0; i < content.length; i++) {
